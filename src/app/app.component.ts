93c676ff0bea1ec9c549d3b82c393aec600df7a8
Alex Coronell <alexcoronell@gmail.com>
2022-04-06 19:10:36 -0500

    Basic structure scss files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'masterisand-angular';
+  title = 'Masterisand | Impresión Digital';
 }
