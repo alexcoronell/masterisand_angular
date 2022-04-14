a117690cf57c66b60fb078f216d425b183c44c97
Alex Coronell <alexcoronell@gmail.com>
2022-04-14 17:06:30 -0500

    Home infopromotions components completed

--- src/app/pages/home/promotions-section/promotions-section.component.ts
+++ src/app/pages/home/promotions-section/promotions-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as data from '../../../../assets/json/promotions.json'
 
 @Component({
   selector: 'app-promotions-section',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromotionsSectionComponent implements OnInit {
 
+  promotions: any = (data as any).default;
+
+  sectionTitle: string = "Infopromociones";
+  numberPromotions: number = this.promotions.length;
+  finalWidth: number = 95 / this.numberPromotions;
+  currentImage: number = 0;
+  total: number = this.numberPromotions - 1;
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.finalWidth);
+    this.changeImages()
+  }
+
+  changeImages() {
+    setInterval(() => {
+      if(this.currentImage == this.total) {
+        this.currentImage = 0
+      } else {
+        this.currentImage += 1;
+      }
+    }, 3000 )
+
+  }
+
+  chargeImage(index: number) {
+    this.currentImage = index;
+  }
+
+  nextImage() {
+    if(this.currentImage == this.total) {
+      this.currentImage = 0
+    } else {
+      this.currentImage += 1;
+    }
+  }
+
+  prevImage() {
+    if(this.currentImage == 0) {
+      this.currentImage = this.total;
+    } else {
+      this.currentImage -= 1;
+    }
   }
 
 }
