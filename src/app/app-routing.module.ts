26628a261f3aca995318020adbb998ab878c1da5
Alex Coronell <alexcoronell@gmail.com>
2022-04-15 10:56:13 -0500

    Services page component route created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 // Pages Components
 import { HomeComponent } from './pages/home/home.component';
 import { BusinessComponent } from './pages/business/business.component';
+import { ServicesComponent } from './pages/services/services.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'la-empresa', component: BusinessComponent }
+  { path: 'la-empresa', component: BusinessComponent },
+  { path: 'servicios', component: ServicesComponent}
 ];
 
 @NgModule({
