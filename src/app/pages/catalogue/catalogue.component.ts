d85adb6b951224d1b2934020e4d120cbd0d6dfee
Alex Coronell <alexcoronell@gmail.com>
2022-04-20 18:31:15 -0500

    Catalogue page component completed

--- src/app/pages/catalogue/catalogue.component.ts
+++ src/app/pages/catalogue/catalogue.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatalogueComponent implements OnInit {
 
+  title: string = 'Catálogo'
+  background: string = 'background-catalogo.jpg';
+
+  catalogueImages: string[] = [
+    'catalogo1.jpg',
+    'catalogo2.jpg',
+    'catalogo3.jpg',
+    'catalogo4.jpg',
+    'catalogo5.jpg',
+    'catalogo6.jpg'
+  ]
+
+  catalogueVideos: string[] = [
+    'impresion-libro-1.mp4',
+    'impresion-libro-2.mp4',
+    'plotterdecorte.mp4'
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
