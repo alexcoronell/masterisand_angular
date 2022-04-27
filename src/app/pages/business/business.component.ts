a9573f0d1020b530f80875b16953041be54be6b5
Alex Coronell <alexcoronell@gmail.com>
2022-04-27 18:58:13 -0500

    Meta tags and title added on page components

--- src/app/pages/business/business.component.ts
+++ src/app/pages/business/business.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import * as data from '../../../assets/json/descriptionBusiness.json';
 
 @Component({
@@ -10,10 +11,24 @@ export class BusinessComponent implements OnInit {
 
   business: any = (data as any).default;
 
-  title: string = 'Masterisand'
+  titlePage: string = 'Masterisand'
   background: string = 'background-quienessomos.jpg';
 
-  constructor() { }
+  constructor(private meta: Meta, private title: Title) {
+    this.meta.addTags([
+      { name: 'description', content: 'Somos una empresa innovadora en servicios para medios impresos publicitarios, y que cuenta con profesionales que poseen una gran trayectoria y experiencia en las artes gráficas, arte final, preprensa, impresión de gran formato, publicidad impresa e impresión digital por demanda, Desarrollo Web y Marketing Digital.' },
+      { name: 'author', content: 'Masterisand' },
+      { name: 'keywords', content: 'Impresion digital, impresion gran formato, diseño y desarrollo web, angula, spa, single page applications, marketing digital, redes sociales, facebook, instagram, google, diseño gráfico, diseno grafico, sublimacion' },
+      { name: 'url', content: 'http://www.masterisand.com.co/' },
+      { name: 'locale', content: 'es_CO' },
+      { name: 'title', content: 'Masterisand | Home Page' },
+      { name: 'site_name', content: 'Masterisand' }
+    ]);
+    this.setTitle('Masterisand - Centro de Impresión y Procesamiento Digital | La Empresa');
+  }
+  public setTitle(newTitle: string) {
+    this.title.setTitle(newTitle);
+  }
 
   ngOnInit(): void {
   }
--- src/app/pages/catalogue/catalogue.component.ts
+++ src/app/pages/catalogue/catalogue.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-catalogue',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatalogueComponent implements OnInit {
 
-  title: string = 'Catálogo'
+  titlePage: string = 'Catálogo'
   background: string = 'background-catalogo.jpg';
 
   catalogueImages: string[] = [
@@ -25,7 +26,21 @@ export class CatalogueComponent implements OnInit {
     'plotterdecorte.mp4'
   ]
 
-  constructor() { }
+  constructor(private meta: Meta, private title: Title) {
+    this.meta.addTags([
+      { name: 'description', content: 'Somos una empresa innovadora en servicios para medios impresos publicitarios, y que cuenta con profesionales que poseen una gran trayectoria y experiencia en las artes gráficas, arte final, preprensa, impresión de gran formato, publicidad impresa e impresión digital por demanda, Desarrollo Web y Marketing Digital.' },
+      { name: 'author', content: 'Masterisand' },
+      { name: 'keywords', content: 'Impresion digital, impresion gran formato, diseño y desarrollo web, angula, spa, single page applications, marketing digital, redes sociales, facebook, instagram, google, diseño gráfico, diseno grafico, sublimacion, catalogo, catálogo, impresion, video' },
+      { name: 'url', content: 'http://www.masterisand.com.co/' },
+      { name: 'locale', content: 'es_CO' },
+      { name: 'title', content: 'Masterisand | Home Page' },
+      { name: 'site_name', content: 'Masterisand' }
+    ]);
+    this.setTitle('Masterisand - Centro de Impresión y Procesamiento Digital | Catálogo');
+  }
+  public setTitle(newTitle: string) {
+    this.title.setTitle(newTitle);
+  }
 
   ngOnInit(): void {
   }
