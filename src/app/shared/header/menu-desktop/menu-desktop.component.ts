6d96433552090bf6318db60ef5e176009cac3637
Alex Coronell <alexcoronell@gmail.com>
2022-05-02 18:49:32 -0500

    Scroll effect applied on menu desktop

--- src/app/shared/header/menu-desktop/menu-desktop.component.ts
+++ src/app/shared/header/menu-desktop/menu-desktop.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import * as data from '../../../../assets/json/menu.json';
 
 @Component({
@@ -11,9 +11,31 @@ export class MenuDesktopComponent implements OnInit {
   menu: any = (data as any).default;
   hiddenMenu: boolean = true;
 
+  scroll:number = 0;
+
+  @ViewChild('buttonMenuDesktop') buttonMenuDesktop: ElementRef;
+  @ViewChild('buttonMenuDesktopClose') buttonMenuDesktopClose: ElementRef;
+  @ViewChild('menuDesktop') menuDesktop: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+    window.addEventListener('scroll', this.scrollEvent, true);
+    this.scroll = window.scrollY;
+  }
+
+  scrollEvent = (event: any): void => {
+    const number = event.srcElement.scrollTop;
+    this.scroll = window.scrollY;
+    if(this.scroll > 50) {
+      this.buttonMenuDesktop.nativeElement.classList.add('buttonDesktopScroll');
+      this.buttonMenuDesktopClose.nativeElement.classList.add('buttonDesktopScroll');
+      this.menuDesktop.nativeElement.classList.add('menuDesktopScroll');
+    } else {
+      this.buttonMenuDesktop.nativeElement.classList.remove('buttonDesktopScroll');
+      this.buttonMenuDesktopClose.nativeElement.classList.remove('buttonDesktopScroll');
+      this.menuDesktop.nativeElement.classList.remove('menuDesktopScroll');
+    }
   }
 
 }
