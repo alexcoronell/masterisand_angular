2054b0982e25a7ed5a689156ed79be08e79d4296
Alex Coronell <alexcoronell@gmail.com>
2022-05-02 18:38:27 -0500

    Scroll effect applied on header background 2

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  scroll:number = 0;
+
+  @ViewChild('background2') background2: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+    window.addEventListener('scroll', this.scrollEvent, true);
+    this.scroll = window.scrollY;
+  }
+
+  scrollEvent = (event: any): void => {
+    const number = event.srcElement.scrollTop;
+    this.scroll = window.scrollY;
+    if(this.scroll > 50) {
+      this.background2.nativeElement.classList.add('Header__background--2--hidden');
+    } else {
+      this.background2.nativeElement.classList.remove('Header__background--2--hidden');
+    }
   }
 
 }
