a6c7379e8742354e92697731f94cbcbfca60fa4a
Alex Coronell <alexcoronell@gmail.com>
2022-05-03 18:10:30 -0500

    Scroll effect added to background 1 in header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -9,6 +9,7 @@ export class HeaderComponent implements OnInit {
 
   scroll:number = 0;
 
+  @ViewChild('background1') background1: ElementRef;
   @ViewChild('background2') background2: ElementRef;
 
   constructor() { }
@@ -22,8 +23,10 @@ export class HeaderComponent implements OnInit {
     const number = event.srcElement.scrollTop;
     this.scroll = window.scrollY;
     if(this.scroll > 50) {
+      this.background1.nativeElement.classList.add('Header__background--1--scroll');
       this.background2.nativeElement.classList.add('Header__background--2--hidden');
     } else {
+      this.background1.nativeElement.classList.remove('Header__background--1--scroll');
       this.background2.nativeElement.classList.remove('Header__background--2--hidden');
     }
   }
